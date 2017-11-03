ed07c550a0703b49bad6380de34b381daef98305
Jonathan Mah <jon@jonmah.com>
2017-11-03 15:27:25 -0700

    Added player object and placeholder scoreboard

--- script.js
+++ script.js
@@ -13,13 +13,33 @@ function playgame() {
 function render() {
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(mainImage, pacmouth, pacdirection, 32, 32, player.x, player.y, 32, 32);
+  context.drawImage(
+    mainImage,
+    player.pacmouth,
+    player.pacdirection,
+    32,
+    32,
+    player.x,
+    player.y,
+    32,
+    32
+  );
+  context.font = '20px Verdana';
+  context.fillStyle = '#fff';
+  context.fillText(`Pacman: ${score} vs Ghost: ${gscore}`, 2, 18);
 }
 
 // Create canvas from JavaScript to ensure that everything is loaded
 // and we are able to connect to the [created] canvas element
-const players = {
+const player = {
+  x: 50,
+  y: 100,
+  pacmouth: 320,
+  pacdirection: 0,
+  size: 32,
 };
+let score = 0;
+let gscore = 0;
 const canvas = document.createElement('canvas');
 var context = canvas.getContext('2d');
 canvas.height = 400;
