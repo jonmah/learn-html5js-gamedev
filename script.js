96ed49a833bd2ee9f93e99522b3f1fa338bcd8af
Jonathan Mah <jon@jonmah.com>
2017-11-03 11:49:13 -0700

    Added some content and styling to the canvas so we have something to look at

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-const canvas = document.getElementById("game-space");
+const canvas = document.getElementById('game-space');
+let context = canvas.getContext('2d');
+
+// Just to see shit on the canvas
+context.fillText('hello world!', 10, 150);
