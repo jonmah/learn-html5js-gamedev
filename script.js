9fd5dbe13a525cc6f79c522eb4acb5fbd0b42760
Jonathan Mah <jon@jonmah.com>
2017-11-03 12:46:52 -0700

    Use JavaScript to generate the canvas instead

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const canvas = document.getElementById('game-space');
-let context = canvas.getContext('2d');
+// const canvas = document.getElementById('game-space');
+// let context = canvas.getContext('2d');
 
-// Just to see shit on the canvas
+// // Just to see shit on the canvas
+// context.fillText('hello world!', 10, 150);
+
+// Create canvas from JavaScript to ensure that everything is loaded
+// and we are able to connect to the [created] canvas element
+const canvas = document.createElement('canvas');
+var context = canvas.getContext('2d');
+canvas.height = 400;
+canvas.width = 600;
+document.body.appendChild(canvas);
 context.fillText('hello world!', 10, 150);
