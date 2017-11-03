848ae801cefcc58984e0cedca87aff410d0625de
Jonathan Mah <jon@jonmah.com>
2017-11-03 12:47:52 -0700

    Remove commented code

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// const canvas = document.getElementById('game-space');
-// let context = canvas.getContext('2d');
-
-// // Just to see shit on the canvas
-// context.fillText('hello world!', 10, 150);
-
 // Create canvas from JavaScript to ensure that everything is loaded
 // and we are able to connect to the [created] canvas element
 const canvas = document.createElement('canvas');
