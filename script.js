f39cd1d8de3be51afde104d652c23026fa8306ec
Jonathan Mah <jon@jonmah.com>
2017-11-03 14:14:05 -0700

    Connect to the canvas and make some small changes

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+const checkReady = () => {
+    // load image and play game
+    this.ready = true;
+    playgame();
+}
+
+// launch the game
+function playgame() {
+    render();
+}
+
+// output content to canvas
+function render() {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 // Create canvas from JavaScript to ensure that everything is loaded
 // and we are able to connect to the [created] canvas element
 const canvas = document.createElement('canvas');
 var context = canvas.getContext('2d');
 canvas.height = 400;
 canvas.width = 600;
+mainImage = new Image();
+mainImage.ready = false;
+mainImage.onload = checkReady;
+mainImage.src = "assets/pac.png";
+
 document.body.appendChild(canvas);
 context.fillText('hello world!', 10, 150);
